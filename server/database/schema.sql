4435f9a1e600b10a9d5ac046b7e7ad7e94a893ec
lmeromy <41759460+lmeromy@users.noreply.github.com>
2021-06-16 13:59:24 -0600

    Update README, update db, add schema file, fix SQL error

---
+++ server/database/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE exercises (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    description VARCHAR(255),
+    category VARCHAR(255)
+);
--- server/models/exercises.js
+++ server/models/exercises.js
@@ -2,18 +2,18 @@ const db = require("../database");
 
 class Exercises {
   static retrieveAll(callback) {
-    db.query("SELECT title, description, category from exercises", function (
-      err,
-      res
-    ) {
-      if (err.error) return callback(err);
-      callback(res);
-    });
+    db.query(
+      "SELECT title, description, category from exercises",
+      function (err, res) {
+        if (err.error) return callback(err);
+        callback(res);
+      }
+    );
   }
 
   static insert(exercise, callback) {
     db.query(
-      "INSERT INTO exercises (title, description, category), VALUES ($1, $2, $3)",
+      "INSERT INTO exercises (title, description, category) VALUES ($1, $2, $3)",
       [exercise.title, exercise.description, exercise.category],
       function (err, res) {
         if (err.error) return callback(err);
