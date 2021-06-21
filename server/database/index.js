98e2058f032669324e6d4b64fbd24e44d77cf0f2
lmeromy <41759460+lmeromy@users.noreply.github.com>
2021-06-21 15:24:25 -0600

    Fuss with index.js for database connection

--- server/database/index.js
+++ server/database/index.js
@@ -4,13 +4,15 @@ const CONNECTION_STRING =
   process.env.DATABASE_URL ||
   "postgresql://postgres:leahmeromy@localhost:5432/hiitathome-db";
 
-// const SSL = process.env.NODE_ENV === "production";
+const SSL = process.env.NODE_ENV === "production";
 
 class Database {
   constructor() {
     this._pool = new Pool({
       connectionString: CONNECTION_STRING,
-      ssl: { rejectUnauthorized: false },
+      // settings for deployed app versus local dev...
+      // ssl: { rejectUnauthorized: false },
+      ssl: SSL,
     });
 
     this._pool.on("error", (err, client) => {
